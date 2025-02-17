ce0c9a7c8f288a68703c0876c5a3163a30c3e40c
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2025-02-17 08:53:58 -0300

    chore: create http server

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import API from "./API"
+import API from "./AccountController"
 import GetAccount from "./GetAccount"
 import { AccountRepositoryDatabase } from "./AccountRepository"
 import Signup from "./Signup"
 import { PgPromiseAdapter } from "./DatabaseConnection";
+import { ExpressAdapter } from "./HttpServer";
+import AccountController from "./AccountController";
 
 const connection = new PgPromiseAdapter();
 const accountRepository = new AccountRepositoryDatabase(connection)
 const signup = new Signup(accountRepository)
 const getAccount = new GetAccount(accountRepository)
-const api = new API(signup, getAccount)
-api.build()
-api.start()
+const httpServer = new ExpressAdapter()
+new AccountController(httpServer, signup, getAccount)
+
+httpServer.listen(3000)
