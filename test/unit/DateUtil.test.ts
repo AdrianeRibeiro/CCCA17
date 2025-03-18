f4fccaf4462994c047d2d28dff77d6b0520029f1
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2025-03-18 08:42:54 -0300

    chore: create domain utils

---
+++ test/unit/DateUtil.test.ts
@@ -0,0 +1,10 @@
+import Period from "../../src/domain/Period";
+import DateUtil from "../../src/domain/Period";
+
+test("Deve calcular a diferença entre duas data em milisegundos", function () {
+  const start = new Date("2023-01-10T10:00:00");
+	const end = new Date("2023-01-10T10:30:00");
+  const period = new Period(start, end)
+  const diff = period.calculateDiffInMili()
+  expect(diff).toBe(1800000)
+})
