7f4f685ced2c97359d70115681e7d1a4b045956f
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2025-03-21 08:58:29 -0300

    chore: add script to start app

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Parando todos os containers..."
+docker stop $(docker ps -q)
+
+echo "Subindo banco e recriando tabelas..."
+docker-compose up -d
+
+echo "Iniciando aplicação..."
+npx nodemon src/main.ts chat
